12d3ab3e57a88e34837363fa82d925066a720e51
Cameron <95046995+Csolo1234@users.noreply.github.com>
2023-12-24 12:10:16 -0500

    message

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return (
+    <nav className="nav">
+        <a href="/" className="cite tiel">Cite</a>
+        <ul>
+            <li>
+             <a href="/princing">princing</a>  
+             </li> 
+             <li>
+             <a href="/about">about</a>
+            </li> 
+        </ul>
+    </nav>
+    )
+}
